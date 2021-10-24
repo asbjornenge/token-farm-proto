bc005921e049c562db811ce2e7a7d2a1a3d5e60e
Asbjørn Enge <asbjorn@hanafjedle.net>
2021-10-24 16:57:55 +0200

    Updated farm and tests

--- farm.js
+++ farm.js
@@ -1,4 +1,4 @@
-class Farm {
+export class Farm {
   constructor(rewards, rewardPerBlock) {
     this.balances = {}
     this.details = {
@@ -6,7 +6,7 @@ class Farm {
       rewardPerBlock: rewardPerBlock,
       endLevel: 0,
       totalStaked: 0,
-      totalStakedLevel: {0: 0},
+      totalStakedLevel: {0:0},
       rewardsDistributed: 0
     }
     this.level = 0
@@ -19,22 +19,24 @@ class Farm {
   stake(address, amount) {
     if (!this.balances[address]) this.balances[address] = {
       balance: 0,
+      balanceLevel: {0:0},
       rewards: 0,
       paid: 0,
       lastLevelPaid: this.level,
     }
-    this.details.totalStaked += amount
     this.balances[address].balance += amount
+    this.balances[address].balanceLevel[this.level] = this.balances[address].balance
+    this.details.totalStaked += amount
     this.details.totalStakedLevel[this.level] = this.details.totalStaked
   }
 
-  findTotalStakedAtLevel(level) {
-    let levels = Object.keys(this.details.totalStakedLevel)
+  findAtLevel(level, _map) {
+    let levels = Object.keys(_map)
     let totalStakeLevel = levels.reduce((foundLevel, l) => {
-      if (l <= level) return l
+      if (l < level) return l
       return foundLevel
     },0)
-    return this.details.totalStakedLevel[totalStakeLevel]
+    return _map[totalStakeLevel]
   }
 
   claim(address, debug=false) {
@@ -44,29 +46,31 @@ class Farm {
     let claimLevel = this.level
     if (claimLevel > this.details.endLevel) claimLevel = this.details.endLevel
     let rewards = 0
-    for (let level = user.lastLevelPaid; level < claimLevel; level++) {
-      let tsl = this.findTotalStakedAtLevel(level)
+    for (let level = user.lastLevelPaid+1; level <= claimLevel; level++) {
+      let tsl = this.findAtLevel(level, this.details.totalStakedLevel)
       let rwt = this.details.rewardPerBlock / tsl
-      let rewardToUser = user.balance * rwt 
+      let bal = this.findAtLevel(level, user.balanceLevel)
+      //let rwt = Math.floor((this.details.rewardPerBlock / tsl) * 100) / 100
+      let rewardToUser = bal * rwt 
+      //console.log(address,level,tsl,rwt,bal,rewardToUser)
       rewards += rewardToUser
       this.details.rewards -= rewardToUser
     }
     user.paid += rewards
-    user.lastLevelPaid = this.level
+    user.lastLevelPaid = claimLevel 
     user.rewards = 0 
   }
 }
 
-const f = new Farm(10000, 100)
-f.start()
-f.stake("user1", 1000)
-f.level = 5
-f.stake("user2", 1000)
-f.level = 50
-f.claim("user1")
-f.level = 100
-f.claim("user2")
-f.claim("user1")
-
-console.log(f.balances)
-console.log(Object.values(f.balances).reduce((tp, u) => tp+=u.paid,0))
+//const TOTAL_REWARDS = 10000
+//const REWARDS_BLOCK = 100
+//const f = new Farm(TOTAL_REWARDS, REWARDS_BLOCK)
+//f.start()
+//
+//f.level =  0; f.stake("user1", 1000)
+//f.level = 10; f.stake("user1", 3000)
+//f.level = 50; f.stake("user2", 1000)
+//f.level = 100
+//f.claim("user1")
+//f.claim("user2")
+//console.log(f.balances)
---
+++ test/test.js
@@ -0,0 +1,44 @@
+import assert from 'assert'
+import { Farm } from '../farm.js'
+
+describe('Rewards', function() {
+  it('Claim should distribute rewards fairly', function() {
+    const TOTAL_REWARDS = 1000
+    const REWARDS_BLOCK = 10
+    const f = new Farm(TOTAL_REWARDS, REWARDS_BLOCK)
+    f.start()
+
+    f.level = 0
+    f.stake("user1", 1000)
+    f.level = 50
+    f.stake("user2", 1000)
+    f.level = 100
+    f.claim("user1")
+    f.claim("user2")
+
+    assert(f.balances.user1.paid == 750)
+    assert(f.balances.user2.paid == 250)
+    const totalPaid = Object.values(f.balances).reduce((tp, u) => tp+=u.paid,0)
+    assert(totalPaid == TOTAL_REWARDS)
+  })
+  it('Users can stake and claim whenever they want', function() {
+    const TOTAL_REWARDS = 10000
+    const REWARDS_BLOCK = 100
+    const f = new Farm(TOTAL_REWARDS, REWARDS_BLOCK)
+    f.start()
+
+    f.level =  0; f.stake("user1", 1000)
+    f.level = 10; f.stake("user1", 2000)
+    f.claim("user1")
+    f.level = 50; f.stake("user2", 1000)
+    f.claim("user1")
+    f.level = 100
+    f.claim("user1")
+    f.claim("user2")
+
+    assert(f.balances.user1.paid == 8750)
+    assert(f.balances.user2.paid == 1250)
+    const totalPaid = Object.values(f.balances).reduce((tp, u) => tp+=u.paid,0)
+    assert(totalPaid == TOTAL_REWARDS)
+  })
+})
